afef0b42d1520ee912f2e8bd73eb015b31429707
ayaidrissi06 <aya18idrissi@gmail.com>
2026-05-12 12:51:35 +0100

    dernier commit

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 console.log("Script chargé avec succès !");
+const messageBtn = document.getElementById("messageBtn");
+const messagePara = document.getElementById("message");
+messageBtn.addEventListener("click", function () {
+    messagePara.textContent = "Vous avez cliqué sur le bouton !";
+});
